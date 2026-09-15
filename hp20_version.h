ed37812302dcf0b64c758a19716dcee1e5f3d97e
EBTW <huynhtandung@embertower.com>
2026-09-15 17:19:19 +0700

    release: bump HP20 to v0.9.7 OTA acceptance candidate

--- hp20_version.h
+++ hp20_version.h
@@ -19,10 +19,10 @@ namespace hp20 { namespace version {
 
 constexpr uint8_t MAJOR = 0;
 constexpr uint8_t MINOR = 9;
-constexpr uint8_t PATCH = 6;
+constexpr uint8_t PATCH = 7;
 
 constexpr const char* TITLE = "HP20";   // ThingsBoard OTA package title must match.
-constexpr const char* STRING = "0.9.6";
-constexpr const char* TAG = "v0.9.6";
+constexpr const char* STRING = "0.9.7";
+constexpr const char* TAG = "v0.9.7";
 
 } }  // namespace hp20::version
